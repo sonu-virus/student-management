50fd2b52e179ac2b8c898a3f8bcf5bdfaa33b4a5
Sonu <lallasonu36@gmail.com>
2024-04-01 11:28:35 +0530

    reset passwor page imlimented

---
+++ src/pages/auth/ResetPassword/index.jsx
@@ -0,0 +1,47 @@
+import React,{useState,} from "react";
+import { useParams } from "react-router-dom";
+
+function ResetPassword() {
+const [password,setPassword] = useState("")
+const params = useParams()
+
+  const resetPassword = async (event) => {
+    event.preventDefault();
+    try {
+      const res = await fetch(
+        `https://api.iot.inflection.org.in/users/reset_password/${params.token}`,
+        {
+          method: "PATCH",
+          body: JSON.stringify({
+            password:password
+          }),
+          headers: {
+            "Content-type": "application/json; charset=UTF-8",
+          },
+        }
+      );
+      const data = await res.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return (
+    <div className="flex items-center justify-center h-screen ">
+      <form className="px-20 py-40 bg-gray-400" onSubmit={resetPassword}>
+        <div className="grid grid-row gap-4">
+          <label> CreatePassword</label>
+          <input
+            type="password"
+            placeholder="EnterPassword"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit">CreatePassword </button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default ResetPassword;
---
+++ src/pages/auth/SignUp/index.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import { useState } from "react";
+// import axios from "axios";
+
+const SignUpPage = () => {
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+
+  const signup = async (event) => {
+    event.preventDefault()
+    try {
+
+      const res = await fetch(
+        "https://api.iot.inflection.org.in/users/signup",
+        {
+          method: "POST",
+          body: JSON.stringify({
+            email: email,
+            full_name: name,
+            gender:'male',
+            reset_password_ui_url:'http://localhost:3000/auth/reset_password'
+          }),
+          headers: {
+            "Content-type": "application/json; charset=UTF-8",
+          },
+        }
+      );
+      const data = await res.json();
+      console.log(data);
+
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <div className="container">
+      <div className="flex bg-gray-600 h-screen justify-center p-4">
+        <form className=" border-2   p-28 bg-purple-600" onSubmit={signup}>
+          <div className="grid grid-row gap-4">
+            <label className="text-2xl ">Email</label>
+            <div className=" border-2  border-blue-400 "> 
+              <input
+              className="p-2"
+                required
+                type="email"
+                placeholder="Enter Email"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+          </div>
+          <div className="grid gap-4 grid-row">
+            <label className="text-2xl  pt-2">Name</label>
+            <div  className=" border-2  border-blue-400 ">
+              <input className="p-2"
+                required
+                type="text"
+                placeholder="Enter Full Name"
+                onChange={(e) => setName(e.target.value)}
+              />
+            </div>
+          </div>
+          <div>
+            <button className=" rounded-md p-2 mt-8 border-2 border-blue-400" type="submit">Create Account</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default SignUpPage;
+
