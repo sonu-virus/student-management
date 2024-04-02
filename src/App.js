1f32710848182e398de367d25fb010de6287a2bb
Sonu <lallasonu36@gmail.com>
2024-04-02 08:34:29 +0530

    login impliment

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import SignUpPage from "./pages/auth/SignUp";
 import HomePage from "./pages/HomePage";
 import ResetPassword from "./pages/auth/ResetPassword";
+import Login from "./pages/auth/LogIn";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
 
         <Route path="/users/signup" element={<SignUpPage />} />
         <Route path="/auth/reset_password/:token" element={<ResetPassword />} />
+        <Route path="/users/login" element={<Login />} />
       </Routes>
     </div>
   );
--- src/pages/auth/LogIn/index.jsx
+++ src/pages/auth/LogIn/index.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React,{useState} from "react";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const login = async (event) => {
+    event.preventDefault();
+    try {
+      const res = await fetch(`https://api.iot.inflection.org.in/users/login`, {
+        method: "POST",
+        body: JSON.stringify({
+          email: email,
+          password: password,
+        }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      });
+      const data = await res.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <div>Login</div>
-  )
+    <div className="flex items-center justify-center h-screen">
+      <form className="px-20 py-32 bg-gray-400" onSubmit={login}>
+        <div className="grid grid-row gap-4">
+            <label>Email</label>
+            <input
+              type="email"
+              placeholder="Enter Your Email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <label>password</label>
+            <input
+              type="password"
+              placeholder="Enter Your password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          <button type="submit">Login</button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
