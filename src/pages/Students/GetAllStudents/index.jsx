afb5a2cc436c87fb5577c2f42d3139491d986467
Deepak Kumar Maurya <deepak04122000maurya@gmail.com>
2024-05-28 15:32:38 +0530

    StudentListCardAdded

--- src/pages/Students/GetAllStudents/index.jsx
+++ src/pages/Students/GetAllStudents/index.jsx
@@ -1,9 +1,9 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { getCookie } from '../../utils/manageCookie';
 
 const StudentsListPage = () => {
-
-const accessToken = getCookie('accessToken')
+  const [AllStuData, setAllStuData] = useState([]);
+  const accessToken = getCookie('accessToken');
 
   useEffect(() => {
     const fetchAllStudents = async () => {
@@ -19,6 +19,7 @@ const accessToken = getCookie('accessToken')
         );
         const data = await res.json();
         console.log(data);
+        setAllStuData(data);
       } catch (err) {
         console.log(err);
       }
@@ -26,7 +27,30 @@ const accessToken = getCookie('accessToken')
     fetchAllStudents();
   }, []);
 
-  return <div>StudentsListPage</div>;
+  return (
+    <div>
+      <div>StudentsListPage</div>
+      <div className="grid grid-cols-3 gap-4 ">
+        {AllStuData?.map((data) => (
+          <>
+            <div className="bg-blue-600 p-2">
+              <p>Name : {data.name}</p>
+              <p>Email : {data.email}</p>
+              <p>Aadhar : {data.aadhar}</p>
+              <p>Adress : {data.address}</p>
+              <p>AlternateMobileNumber : {data.alternate_mobile_number}</p>
+              <p>DateOfBirth : {data.dob}</p>
+              <p>FatherName : {data.father_name}</p>
+              <p>Gender : {data.gender.gender}</p>
+              <p>MobileNumber : {data.mobile_number}</p>
+              <p>MotherName : {data.mother_name}</p>
+              <p>Category : {data.category.student_categories}</p>
+            </div>
+          </>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default StudentsListPage;
