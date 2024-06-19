c63d01c4148f4861197adf04fae4db6e4e25e220
Deepak Kumar Maurya <deepak04122000maurya@gmail.com>
2024-06-19 14:22:52 +0530

    layout added

--- src/pages/Batch/GetAllBatches/index.jsx
+++ src/pages/Batch/GetAllBatches/index.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { getCookie } from '../../../utils/manageCookie';
 import { useParams, Link } from 'react-router-dom';
+import AdminLayout from '../../Components/AdminLayout/Layout';
 
 const GetAllBatches = () => {
   const params = useParams();
@@ -33,22 +34,24 @@ const GetAllBatches = () => {
   }, []);
 
   return (
-    <div>
-      <p className="pt-4 pl-4 text-3xl text-blue-600">Batches</p>
-      <div className="flex flex-col gap-y-4 m-6">
-        {batchDetails.map((data) => (
-          <li
-            key={data.id}
-            className="flex font-serif text-lg hover:text-blue-600 border-2 p-2 border-black  justify-between"
-          >
-            <p> BatchName : {data.name} </p>
-            <p> Class : {data.class} </p>
-            <p> Note : {data.note} </p>
-            <Link to={`/batches/${data.id}`}>EditBtatch</Link>
-          </li>
-        ))}
+    <AdminLayout>
+      <div className="w-full">
+        <p className="pt-4 pl-4 text-3xl text-blue-600">Batches</p>
+        <div className="flex flex-col gap-y-4 m-6">
+          {batchDetails.map((data) => (
+            <li
+              key={data.id}
+              className="flex font-serif text-lg hover:text-blue-600 border-2 p-2 border-black  justify-between"
+            >
+              <p> BatchName : {data.name} </p>
+              <p> Class : {data.class} </p>
+              <p> Note : {data.note} </p>
+              <Link to={`/batches/${data.id}`}>EditBtatch</Link>
+            </li>
+          ))}
+        </div>
       </div>
-    </div>
+    </AdminLayout>
   );
 };
 
